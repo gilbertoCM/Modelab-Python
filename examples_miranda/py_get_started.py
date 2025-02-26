3c8d1068e167ab56591b689e22eafedc4bec7481
miranda <mirandamo.o26@gmail.com>
2025-02-26 13:04:30 -0700

    primeros ejercicios

---
+++ examples_miranda/py_get_started.py
@@ -0,0 +1,14 @@
+print ("hello world")
+import sys
+
+print(sys.version)
+
+#python will give you an error if you skip the indentation in the print statement
+if 5 > 4
+    print("Five is greater than four!")    
+    print("Five is greater than four!")
+
+#python has no command for declaring a variable 
+x = 5
+y ="Hello, Miranda"
+
